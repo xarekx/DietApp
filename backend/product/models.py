bd858617a5e922659623a5d18ff76843ddb37a8a
Arkadiusz Tarnowski <arektarnowski4@gmail.com>
2024-02-19 14:51:49 +0100

    added ordering in product model

--- backend/product/models.py
+++ backend/product/models.py
@@ -15,6 +15,7 @@ class Product(models.Model):
     class Meta:
         verbose_name = _("Product")
         verbose_name_plural = _("Products")
+        ordering = ['name']
 
     def __str__(self):
         return self.name
