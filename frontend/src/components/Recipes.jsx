77ca77523d35982d5a37c03e8a91accacd0d020e
Arkadiusz Tarnowski <arektarnowski4@gmail.com>
2024-02-29 14:47:18 +0100

    create basic template in recipes List

--- frontend/src/components/Recipes.jsx
+++ frontend/src/components/Recipes.jsx
@@ -5,30 +5,6 @@ import React from 'react';
 
 export function RecipesData() {
 
-    // const newRecipe = {
-    //     "id": 2,
-    //     "title": "Naleśniki z serem i owocami",
-    //     "ingredients": [
-    //         {
-    //             "product_name": "Ser twarogowy chudy",
-    //             "quantity": 125
-    //         },
-    //         {
-    //             "product_name": "Naleśniki",
-    //             "quantity": 180
-    //         },
-    //         {
-    //             "product_name": "Jogurt naturalny",
-    //             "quantity": 100
-    //         },  
-    //         {
-    //             "product_name": "Truskawki",
-    //             "quantity": 75
-    //         }
-    //     ]
-    // }
-
-
     const [recipes, setRecipes] = useState([]);
 
     useEffect(() => {
@@ -39,37 +15,29 @@ export function RecipesData() {
         });
     }, []);
     
-    //   Dodawanie receptury
-    // const handleAddRecipe = () => {
-    //     const newRecipes = [ ... recipes];
-    //     newRecipes.push(newRecipe);
-    //     setRecipes(newRecipes);
-    // };
-
-
-    
     return (
         <>
-        <div className="">
-            <ul>
-                {recipes.map((recipe, index ) => {
-                    return (
-                        <li key={index}>
-                                {recipe.title}
-                                <ul>
-                                    {recipe.ingredients.map((ingredient, index) => 
-                                        <li key={index}>    
-                                        {ingredient.product_name} - {ingredient.quantity}g
-                                        </li>
-                                    )}
-                                </ul>
-                        </li>
-                    );
-                })}
-            </ul>
-        </div>
+            <div className="grid grid-flow-col grid-cols-6 ms-auto me-auto">
+                {recipes.map((recipe, recipeIndex) => {
+                    return(
+                        <div class="max-w-sm h-fit rounded overflow-hidden shadow-lg " key={recipeIndex}>
+                            <img class="w-full" src="https://v1.tailwindcss.com/img/card-top.jpg" alt="Sunset in the mountains"/>
+                            <div class="px-6 py-4">
+                                <div class="font-bold text-xl mb-2">{recipe.title}</div>
+                                <p class="text-gray-700 text-base">
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
+                                </p>
+                                <p className="float-right ">
+                                    <button className="border border-black p-1">Click me</button>
+                                </p>
+                            </div>
+                            <div class="clear-both   px-6 pt-4 pb-2">
+                            </div>
+                        </div>
+                    )
+                })};
+            </div>
         </>
-
     );
 
 }
