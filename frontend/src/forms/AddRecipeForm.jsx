8996af631ca6d4de55fb99b8ae16ccf19c26da1d
Arkadiusz Tarnowski <arektarnowski4@gmail.com>
2024-03-06 09:16:03 +0100

    create basic recipe view with working form

--- frontend/src/forms/AddRecipeForm.jsx
+++ frontend/src/forms/AddRecipeForm.jsx
@@ -28,7 +28,14 @@ export function AddRecipeForm() {
 
     const [products, setProducts] = useState([]);
     const [filteredProducts, setFilteredProducts] = useState(products)
-    const [ingredients, setIngredients] = useState([])
+    const [ingredients, setIngredients] = useState(useState(
+        {
+            product_name:"",
+            quantity:null
+        }))
+
+    const [productValue, setProductValue] = useState();
+    const [quantityValue, setQuantityValue] = useState();
 
 
     const [recipeForm, setRecipeForm] = useState(useState({
@@ -48,11 +55,8 @@ export function AddRecipeForm() {
     };
 
     const handleUpdateIngredientsList = (product) => {
-        setIngredients([...ingredients, product.id]);
+        setProductValue(product);
         setFilteredProducts([]);
-
-        let filterIngredients = document.getElementById("filterIngredients");
-        filterIngredients.value = "";
 
     }
 
@@ -70,19 +74,32 @@ export function AddRecipeForm() {
         setFilteredProducts(products.filter(d => (d.name.toLowerCase().includes(event.target.value))))
     }
 
+    const handleIngredients = (e,product_name, quantity) => {
+        e.preventDefault();
+        setIngredients([...ingredients, {product_name,quantity}])
+        setProductValue()
+        setQuantityValue()
+        
+    }
+
+    // TODO - create recipe form
+
     return (
     <div className="flex flex-col ms-auto me-auto w-2/3 mt-10 rounded-t shadow-xl bg-white h-fit">
         <form className="flex flex-col text-black">
             <label htmlFor="title" className="p-2">Recipe Name</label>
             <input id="title" type="text" value={recipeForm.title} onChange={handleChange} className="bg-slate-100 ps-2 ms-2"></input>
-            <input id="filterIngredients" className="ps-2 ms-2" type="text" onChange={handleFilter}/>
+            <input id="filterIngredients" className="ps-2 ms-2 text-black" type="text" onChange={handleFilter} value={productValue}/>
                 <div className="relative">
-                    <ul className="absolute w-full">
+                    <ul className="absolute w-2/3">
                     {filteredProducts.map((product) => {
                         return(
-                        <li className="bg-white p-2" onClick={()=>handleUpdateIngredientsList(product)}>{product.name}</li>)})}
+                        <li className="bg-white p-2" onClick={()=>handleUpdateIngredientsList(product.name)}>{product.name}</li>)})}
                     </ul>
                 </div>
+            <label htmlFor="quantity">Quantity</label>   
+            <input type="number" placeholder="Quantity" onChange={(e)=> setQuantityValue(e.target.value)} defaultValue={0}/>
+            <button className="border" onClick={(e)=> handleIngredients(e,productValue,quantityValue)}>Add Ingredient</button>
         </form>
         <div>
             <span>
@@ -90,7 +107,7 @@ export function AddRecipeForm() {
             </span>
             <div>
                 <ul>
-                    <li>{ingredients}</li>
+                    {ingredients.map((ingredient) => {return(<li>{ingredient.product_name} {ingredient.quantity}</li>)})}
                 </ul>
                 
             </div>
