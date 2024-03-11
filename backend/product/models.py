c8f25f5023ebe1edb43408ef0c2eed3d6388bab8
Arkadiusz Tarnowski <arektarnowski4@gmail.com>
2024-03-11 14:27:35 +0100

    Create method to create new recipe with ingredients

--- backend/product/models.py
+++ backend/product/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 from django.urls import reverse
-from recipe.models import Recipe
 
 # Create your models here.
 class Product(models.Model):
--- backend/recipe/serializers.py
+++ backend/recipe/serializers.py
@@ -2,13 +2,22 @@ from rest_framework import serializers
 from .models import Recipe
 from ingredients.serializers import IngredientSerializer
 from product.serializers import ProductSerializer
+from ingredients.models import Ingredient
+from product.models import Product
 
 class RecipeSerializer(serializers.ModelSerializer):
     
     ingredients = IngredientSerializer(many=True)
-
     
     class Meta:
         model = Recipe
-        fields = ("id", "title", "ingredients",)
-        depth = 2
+        fields = ['title', 'ingredients']
+        
+    def create(self, validated_data):
+        ingredients_data = validated_data.pop('ingredients')
+        recipe = Recipe.objects.create(**validated_data)
+        for ingredient_data in ingredients_data:
+            product_data = ingredient_data.pop('product')
+            product, _ = Product.objects.get_or_create(**product_data)
+            Ingredient.objects.create(recipe=recipe, product=product, **ingredient_data)
+        return recipe    
