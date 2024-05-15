d52cfe5e1a04ad30ccb87d8d19faae5017c07be5
xarekx <arektarnowski4@gmail.com>
2024-05-15 13:23:07 +0200

    Create post request to logout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -15,6 +15,7 @@ import { UserLogin } from "./components/Login";
 function App() {
 
   const [ recipeToggle, recipeSetToggle ] = useState(false);
+  const [ currentUser, setCurrentUser] = useState(false);
   
   const getCookie = (name) => {
     const value = `; ${document.cookie}`;
@@ -24,6 +25,28 @@ function App() {
       return parts.pop().split(';').shift();
   }
 
+  const handleCurrentUser = (userStatus) => {
+    setCurrentUser(userStatus);
+    console.log(currentUser);
+  }
+
+  const submitLogout = (e) => {
+    e.preventDefault();
+    const csrftoken = getCookie('csrftoken');
+    fetch("http://127.0.0.1:8000/api/user/logout", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                'X-CSRFToken': csrftoken
+            },
+            credentials: 'include'
+        }
+      ).then((response) => {
+        if (response.ok) {
+          setCurrentUser(false);
+        }
+      }).catch((error) => {console.error("Something went wrong with logout", error)})
+  }
 
   return (<>
 
@@ -38,10 +61,21 @@ function App() {
                       <PiBowlFoodFill className="w-10 h-10"/>
                     </Link>
                   </div>
-                  <div className="flex items-center">
-                    <Link to={'/login'} className="text-gray-800 text-sm mr-4">LOGIN</Link> 
-                    <Link to={'/register'} className="bg-indigo-600 px-4 py-2 rounded text-white hover:bg-indigo-500 text-sm">SIGN UP</Link> 
-                  </div>
+                  {currentUser ? 
+                  ( 
+                    (<>
+                    <div className="flex items-center">
+                      <div className="text-gray-800 text-sm mr-4">USERNAME</div> 
+                      <Link to={'/logout'} className="bg-indigo-600 px-4 py-2 rounded text-white hover:bg-indigo-500 text-sm" onClick={(e)=> submitLogout(e)}>Logout</Link> 
+                    </div>
+                    </>)):
+                  (
+                    <div className="flex items-center">
+                      <Link to={'/login'} className="text-gray-800 text-sm mr-4">LOGIN</Link> 
+                      <Link to={'/register'} className="bg-indigo-600 px-4 py-2 rounded text-white hover:bg-indigo-500 text-sm">SIGN UP</Link> 
+                    </div>
+                  )}
+                  
                 </div>
               </div>
             </nav>
@@ -90,7 +124,7 @@ function App() {
               <Route path='/recipes' element={<RecipesData getCookie={getCookie} />}/>
               <Route path='/recipes/:recipeId' element={<RecipeDetails getCookie={getCookie} />}/>
               <Route path='/recipes/add' element={<AddRecipeForm getCookie={getCookie} />}/>
-              <Route path='/login' element={<UserLogin getCookie={getCookie}/>}/>
+              <Route path='/login' element={<UserLogin getCookie={getCookie} userStatus={handleCurrentUser} />}/>
           </Routes>
           </div>
         </main>
--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,14 +1,14 @@
 import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 
 
-export function UserLogin({getCookie}) {
+export function UserLogin({getCookie, userStatus}) {
 
-  const [ currentUser, setCurrentUser ] = useState();
-  const [ registrationToggle, setRegistrationToggle ] = useState(false);
   const [ email, setEmail ] = useState('');
-  const [ username, setUsername ] = useState('');
+  // const [ userStatus, setUserStatus ] = useState(false);
   const [ password, setPassword ] = useState('');
+  const navigate = useNavigate();
 
   const submitLogin = (e) => {
     e.preventDefault();
@@ -27,19 +27,16 @@ export function UserLogin({getCookie}) {
         }
       ).then((response) => {
         if (response.ok) {
-          setCurrentUser(true);
+          userStatus(true);
+          navigate('/');
         } else {
           console.log('Incorrect email or password.');
         }
         }
       ).catch((err) => console.error("There was a problem with post request", err))
-
-
-
   }
-
-  if (!currentUser) {
-    return (
+   
+  return (
       <div className="flex justify-center ms-auto me-auto">
         <div className="py-6 px-8 h-80 mt-20 bg-white rounded shadow-xl">
           <form onSubmit={(e) => submitLogin(e)}>
@@ -57,28 +54,4 @@ export function UserLogin({getCookie}) {
         </div>
       </div>
     );
-  } 
-  // else {
-  //   // Użytkownik jest zalogowany, więc możesz wykonać żądanie GET na produkty
-  //   return <ProductsData />;
-  // }
-
-    // return(
-    //         <div className="flex justify-center ms-auto me-auto">
-    //          <div className="py-6 px-8 h-80 mt-20 bg-white rounded shadow-xl">
-    //            <form onSubmit={(e)=>submitLogin(e)}>
-    //              <div>
-    //                 <label for="email" className="block text-gray-800 font-bold">Email:</label>
-    //                 <input type="text" name="email" id="email" placeholder="@email" className="w-full border border-gray-300 py-2 pl-3 rounded mt-2 outline-none focus:ring-indigo-600 :ring-indigo-600" onChange={e => setEmail(e.target.value)}/>
-    //              </div>
-    //              <div className="mb-6">
-    //                 <label for="password" className="block text-gray-800 font-bold">Password</label>
-    //                 <input type="password" name="password" id="password" placeholder="Password" className="w-full border border-gray-300 py-2 pl-3 rounded mt-2 outline-none focus:ring-indigo-600 :ring-indigo-600" onChange={e => setPassword(e.target.value)} />
-    //                 <a href="#" className="text-sm font-thin text-gray-800 hover:underline mt-2 inline-block hover:text-indigo-600">Forget Password</a>
-    //              </div>
-    //              <button className="cursor-pointer py-2 px-4 block mt-6 bg-indigo-500 text-white font-bold w-full text-center rounded" type="submit">Login</button>
-    //            </form>
-    //          </div>
-    //        </div>
-    // )
 }
