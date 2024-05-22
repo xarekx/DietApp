5c0e605bc68836aa3cfdfd33263e53d7090410f4
xarekx <arektarnowski4@gmail.com>
2024-05-22 14:39:41 +0200

    Added user's username on the application page

--- backend/user_api/views.py
+++ backend/user_api/views.py
@@ -4,7 +4,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from .serializers import UserLoginSerializer, UserRegisterSerializer, UserSerializer
 from rest_framework import permissions, status
-from .validations import validate_email, custom_validation, validate_password
+from .validations import validate_email, custom_validation, validate_password, validate_username
 
 class UserRegister(APIView):
     permission_classes = (permissions.AllowAny, )
@@ -33,8 +33,10 @@ class UserLogin(APIView):
             
             user = serializer.check_user(data)
             login(request,user)
+
+            user_data = UserSerializer(user).data
             
-            return Response(serializer.data, status=status.HTTP_200_OK)
+            return Response({'user':user_data}, status=status.HTTP_200_OK)
         
 class UserLogout(APIView):
     permission_classes = (permissions.AllowAny, )
