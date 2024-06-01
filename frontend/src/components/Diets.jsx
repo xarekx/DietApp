9b4c6f68f51dab8932fbe41313a2e9445ebda874
xarekx <arektarnowski4@gmail.com>
2024-06-01 12:19:12 +0200

    Resolved bug, user got error when any meal was null

--- frontend/src/components/Diets.jsx
+++ frontend/src/components/Diets.jsx
@@ -23,50 +23,48 @@ export function DietsData({getCookie}) {
 
     }, [getCookie])
 
-    console.log(diets);
-
 
 
     return (
-        <div className="flex flex-col lg:w-11/12 ms-auto pe-[vw] h-fit text-xs lg:text-lg">
+        <div className="flex flex-col lg:w-11/12 ms-auto pe-[5vw] h-fit text-xs lg:text-lg ">
             <div className="grid lg:grid-cols-7 gap-x-4 text-center w-5/6 md:w-2/3 lg:w-full mt-[4vh] shadow-2xl">
                 {diets.map((diet, index) => {
                     return(
                         <>
-                        <div className="flex flex-col border-dotted border-r-[1px] border-slate-300">
+                        <div className="flex flex-col border-dotted border-r-[1px] border-slate-300" >
                             <span>{diet.day}</span>
                             <div className="flex flex-col max-w-sm rounded shadow-lg md:text-xs lg:text-sm">
                                 <img className="w-full" src="https://v1.tailwindcss.com/img/card-top.jpg" alt="Sunset in the mountains" />
                                 <div className="">
-                                    <div class="font-bold m-1 mb-4 text-center md:p-0.5 lg:p-1 h-6 md:h-14 md:m-2 lg:m-1">{diet.breakfast.title}</div>
+                                    <div className="font-bold m-1 mb-4 text-center md:p-0.5 lg:p-1 h-6 md:h-14 md:m-2 lg:m-1">{diet.breakfast && diet.breakfast.title ? diet.breakfast.title : 'null'}</div>
                                 </div>
                             </div>
 
                             <div className="flex flex-col max-w-sm rounded shadow-lg md:text-xs lg:text-sm">
                                 <img className="w-full" src="https://v1.tailwindcss.com/img/card-top.jpg" alt="Sunset in the mountains" />
                                 <div className="">
-                                    <div class="font-bold m-1 mb-4 text-center md:p-0.5 lg:p-1 h-6 md:h-14 md:m-2 lg:m-1">{diet.second_breakfast.title}</div>
+                                    <div className="font-bold m-1 mb-4 text-center md:p-0.5 lg:p-1 h-6 md:h-14 md:m-2 lg:m-1">{diet.second_breakfast && diet.second_breakfast.title ? diet.second_breakfast.title : 'null'}</div>
                                 </div>
                             </div>
 
                             <div className="flex flex-col max-w-sm rounded shadow-lg md:text-xs lg:text-sm">
                                 <img className="w-full" src="https://v1.tailwindcss.com/img/card-top.jpg" alt="Sunset in the mountains" />
                                 <div className="">
-                                    <div class="font-bold m-1 mb-4 text-center md:p-0.5 lg:p-1 h-6 md:h-14 md:m-2 lg:m-1">{diet.lunch.title}</div>
+                                    <div className="font-bold m-1 mb-4 text-center md:p-0.5 lg:p-1 h-6 md:h-14 md:m-2 lg:m-1">{diet.lunch && diet.lunch.title ? diet.lunch.title : 'null'}</div>
                                 </div>
                             </div>
 
                             <div className="flex flex-col max-w-sm rounded shadow-lg md:text-xs lg:text-sm">
                                 <img className="w-full" src="https://v1.tailwindcss.com/img/card-top.jpg" alt="Sunset in the mountains" />
                                 <div className="">
-                                    <div class="font-bold m-1 mb-4 text-center md:p-0.5 lg:p-1 h-6 md:h-14 md:m-2 lg:m-1">{diet.afternoon_meal.title}</div>
+                                    <div className="font-bold m-1 mb-4 text-center md:p-0.5 lg:p-1 h-6 md:h-14 md:m-2 lg:m-1">{diet.afternoon_meal && diet.afternoon_meal.title ? diet.afternoon_meal.title : 'null'}</div>
                                 </div>
                             </div>
 
                             <div className="flex flex-col max-w-sm rounded shadow-lg md:text-xs lg:text-sm">
                                 <img className="w-full" src="https://v1.tailwindcss.com/img/card-top.jpg" alt="Sunset in the mountains" />
                                 <div className="">
-                                    <div class="font-bold m-1 mb-4 text-center md:p-0.5 lg:p-1 h-6 md:h-14 md:m-2 lg:m-1">{diet.dinner.title}</div>
+                                    <div className="font-bold m-1 mb-4 text-center md:p-0.5 lg:p-1 h-6 md:h-14 md:m-2 lg:m-1">{diet.dinner && diet.dinner.title ? diet.dinner.title : 'null'}</div>
                                 </div>
                             </div>
 
