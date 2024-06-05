1686999ce6a9ebe0fcdf0a3f0a16e492eab66a3a
xarekx <arektarnowski4@gmail.com>
2024-06-05 14:09:33 +0200

    Prepared an endpoint to get products for a specific day based on user id

--- backend/diets/views.py
+++ backend/diets/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
 from .models import Diet
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from rest_framework import viewsets
 from .serializers import DietSerializer
 from rest_framework.permissions import IsAuthenticated
@@ -10,5 +11,20 @@ class DietView(viewsets.ModelViewSet):
     
     def get_queryset(self):
         user = self.request.user
-        return Diet.objects.filter(user=user)
+        queryset = Diet.objects.filter(user=user)
+        
+        return queryset
+    
+    @action(detail=False, methods=['get'], url_path='recipes-by-day')
+    def recipes_by_day(self, request):
+        user = request.user
+        # list of day as a parameter
+        days = request.query_params.getlist('day')
+        if days:
+            diets = Diet.objects.filter(user=user, day__in=days)
+        else:
+            diets = Diet.objects.filter(user=user)
+
+        serializer = self.get_serializer(diets, many=True)
+        return Response(serializer.data)
 
