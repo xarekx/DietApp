ec211fea2fd64f9fdcc885b70b598cbe748d63f4
xarekx <arektarnowski4@gmail.com>
2024-06-06 12:39:50 +0200

    Created first version of calendar in the GeneratePDF component to generate products from date range

--- frontend/src/components/diets/GeneratePDF.jsx
+++ frontend/src/components/diets/GeneratePDF.jsx
@@ -1,8 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import { PDFDownloadLink, Page, Text, View, Document, StyleSheet } from '@react-pdf/renderer';
+import { Calendar } from 'react-calendar'
+import 'react-calendar/dist/Calendar.css';
 
 
 export function GeneratePdf() {
+
+  const [calendarToggle, setCalendarToggle] = useState(false);
 
 // Tworzenie stylów
 const styles = StyleSheet.create({
@@ -33,10 +37,48 @@ const MyDocument = () => (
 
 
   return (
-    <PDFDownloadLink document={<MyDocument />} 
-    fileName="mypdf.pdf" className="bg-emerald-600 px-4 py-2 rounded text-white hover:bg-emerald-500 text-sm w-fit mt-[3vw] lg:mt-[2vw] ms-auto">
-      {({ blob, url, loading, error }) => (loading ? 'Loading document...' : 'Download Diet')}
-    </PDFDownloadLink>
+    <>
+    <button className="bg-emerald-600 px-4 py-2 rounded text-white hover:bg-emerald-500 text-sm w-fit mt-[3vw] lg:mt-[2vw] ms-auto" onClick={()=> setCalendarToggle(!calendarToggle)}>Get Products</button>
+    {calendarToggle ? 
+      ( 
+        <>
+                <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
+                    <div className="relative w-auto my-6 mx-auto max-w-sm">
+                        {/*content*/}
+                        <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+                            {/*header*/}
+                            <div className="flex p-5 border-b border-solid border-blueGray-200 rounded-t w-full justify-center">
+                                <h3 className="text-3xl font-semibold">
+                                  Which days
+                                </h3>
+                            </div>
+                            {/*body*/}
+                            <div className="relative p-6 flex-auto">
+                              <Calendar/>
+                              <div className="flex items-center justify-end p-6 border-t border-solid border-blueGray-200 rounded-b">
+                                <button
+                                    className="text-red-500 hover:text-red-300 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                                    type="button"
+                                    onClick={() => setCalendarToggle(false)}>
+                                    Close
+                                </button>
+                                <PDFDownloadLink document={<MyDocument />} 
+                                fileName="mypdf.pdf" className="bg-emerald-600 px-4 py-2 rounded text-white hover:bg-emerald-500 text-sm w-fit ms-auto">
+                                  {({ blob, url, loading, error }) => (loading ? 'Loading document...' : 'Download Diet')}
+                                </PDFDownloadLink>
+                                </div>
+                            </div>
+                            {/*footer*/}
+                        </div>
+                    </div>
+                </div>
+                <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+            </>
+      ):null
+    }
+    
+    
+    </>
   )
 
 }
