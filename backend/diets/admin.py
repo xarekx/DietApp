7750de54d2097e16cf659a0fac97ac6020ee8884
xarekx <arektarnowski4@gmail.com>
2024-06-07 18:13:40 +0200

    Created two fields, start_diet_date, end_diet_date - it stores information how long diet last

--- backend/diets/admin.py
+++ backend/diets/admin.py
@@ -5,5 +5,5 @@ from django.contrib.auth.models import User
 
 @admin.register(Diet)
 class Diet(admin.ModelAdmin):
-    list_display = ["id", "user"]
+    list_display = ["id", "user", 'start_diet_date', 'end_diet_date', 'is_active']
     list_filter = ["user"]
