46c257bae0f322c786139b9d0747f92feac9803c
xarekx <arektarnowski4@gmail.com>
2024-06-09 13:28:20 +0200

    Changed date field in day attribute in Diets Model

--- backend/diets/models.py
+++ backend/diets/models.py
@@ -3,23 +3,12 @@ from django.utils.translation import gettext_lazy as _
 from django.utils.timezone import now
 from recipe.models import Recipe
 from user_api.models import AppUser
-# Create your models here.
+
 class Diet(models.Model):
 
-    # To change ( future change, day one, day two, etc. ) - diet will not always start from the monday
-    # // TODO Change the date format
-    DAY_OF_WEEK_CHOICES = [
-        ('monday', 'Monday'),
-        ('tuesday', 'Tuesday'),
-        ('wednesday', 'Wednesday'),
-        ('thursday', 'Thursday'),
-        ('friday', 'Friday'),
-        ('saturday', 'Saturday'),
-        ('sunday', 'Sunday'),
-    ]
 
     user = models.ForeignKey(AppUser, on_delete=models.CASCADE, null=True)
-    day = models.CharField(_('day'), max_length=20, choices=DAY_OF_WEEK_CHOICES, default=DAY_OF_WEEK_CHOICES[0])
+    day = models.DateField(_('day'), default=now)
     breakfast = models.ForeignKey(Recipe, related_name='breakfast_recipe', on_delete=models.CASCADE, null=True, blank=True)
     second_breakfast = models.ForeignKey(Recipe, related_name='second_breakfast_recipe', on_delete=models.CASCADE, null=True, blank=True)
     lunch = models.ForeignKey(Recipe, related_name='lunch_recipe', on_delete=models.CASCADE, null=True, blank=True)
