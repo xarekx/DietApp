d2dfc096820429dfab5d220d6d10f6a0b85cbb56
xarekx <arektarnowski4@gmail.com>
2024-06-10 15:02:47 +0200

    Created possibility to generate PDF document with products - User choosing date range and getting products in that range

--- frontend/src/components/diets/GeneratePDF.jsx
+++ frontend/src/components/diets/GeneratePDF.jsx
@@ -1,68 +1,120 @@
 import React, { useState } from "react";
-import { PDFDownloadLink, Page, Text, View, Document, StyleSheet } from '@react-pdf/renderer';
+import { PDFDownloadLink, Page, Text, View, Document, StyleSheet, Font } from '@react-pdf/renderer';
 import { Calendar } from 'react-calendar'
-import 'react-calendar/dist/Calendar.css';
 
+import 'react-calendar/dist/Calendar.css';
 
 export function GeneratePdf({getCookie}) {
 
   const [calendarToggle, setCalendarToggle] = useState(false);
   const [calendarRange, setCalendarRange] = useState([new Date(), new Date()]);
-  // const [ dietData, setDietData] = useState([]);
+  const [ingredientsData, setIngredientsData] = useState([]);
 
   // destructure calendarRange
   const [startDate, endDate] = calendarRange;
 
+  Font.register({
+    family: 'Roboto',
+    fonts: [
+      {
+        src: '/fonts/Roboto-Regular.ttf',
+        fontWeight: 400,
+      },
+    ]
+  })
+
 // Creating styles of pdf document
 const styles = StyleSheet.create({
-  page: {
-    flexDirection: 'row',
-    backgroundColor: '#E4E4E4'
+  page:{
+      padding: 30,
+      fontFamily: 'Roboto',
+      color:'black'
   },
-  section: {
-    margin: 10,
-    padding: 10,
-    flexGrow: 1
+  table: {
+    flexDirection:'col',
+    display: 'table',
+    width: 'auto',
+    borderRightWidth: 0,
+    borderBottomWidth: 0,
+  },
+  tableRow: {
+    margin: 'auto',
+    flexDirection: 'row',
+  },
+  tableCol: {
+    width: '50%',
+    borderLeftWidth: 0,
+    borderTopWidth: 0,
+  },
+  tableCell: {
+    margin: 'auto',
+    marginTop: 5,
+    fontSize: 10,
+  },
+  tableHeader: {
+    backgroundColor: '#d3d3d3',
+  },
+  header: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: 20,
   }
 });
 
-// const fetchProductsInCalendarRange = () => {
-//   const csrftoken = getCookie('csrftoken')
-//   fetch('http://127.0.0.1:8000/api/diets/recipe-by-day/?day=2024-10-06',{
-//     method:"GET",
-//     headers: {
-//       "Content-Type": "application/json",
-//       'X-CSRFToken': csrftoken
-//     },
-//     credentials:'include'
-//   })
-//   .then(res =>res.json())
-//   .then(data => setDietData(data))
-//   .catch(error => console.error('Error fetching products: ', error));
-// }
+const fetchProductsInCalendarRange = () => {
+  const csrftoken = getCookie('csrftoken');
+        fetch(`http://127.0.0.1:8000/api/diets/products-by-day/?start_date=${validDate(startDate)}&end_date=${validDate(endDate)}`, {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+                'X-CSRFToken': csrftoken
+            },
+            credentials: "include"
+        })
+        .then(res =>res.json())
+        .then(data => setIngredientsData(data))
+        .catch(error => console.error('Error fetching ingredients: ', error));
+}
 
 const validDate = (date) => {
   const realMonth = date.getMonth()+1;
   return date.getFullYear() + '-' + (realMonth < 10 ? '0' + (realMonth) : realMonth) + '-' + (date.getDate() < 10 ? '0' + (date.getDate()) : date.getDate());
 }
 
-
-// Tworzenie komponentu dokumentu
+// Creating pdf Document
 const MyDocument = () => (
   <Document>
     <Page size="A4" style={styles.page}>
-      <View style={styles.section}>
-        <Text>Diet</Text>
-      </View>
-      <View style={styles.section}>
-        <Text>Section #2</Text>
-      </View>
-      <View style={styles.section}>
+      <View style={styles.header}>
+        <Text>List of Products</Text>
         <Text>{validDate(startDate)} - {validDate(endDate)}</Text>
+      </View>
+      <View style={styles.table}>
+        <View style={[styles.tableRow, styles.tableHeader]}>
+          <View style={styles.tableCol}>
+            <Text style={styles.tableCell}>Nazwa Produktu</Text>
+          </View>
+          <View style={styles.tableCol}>
+            <Text style={styles.tableCell}>Ilość (g)</Text>
+          </View>
+        </View>
+        {ingredientsData.map((ingredient, index) => (
+          <View style={styles.tableRow} key={index}>
+            <View style={styles.tableCol}>
+              <Text style={styles.tableCell}>{ingredient.name}</Text>
+            </View>
+            <View style={styles.tableCol}>
+              <Text style={styles.tableCell}>{ingredient.total_quantity}</Text>
+            </View>
+          </View>
+        ))}
       </View>
     </Page>
   </Document>
 );
+
+
 
   return (
     <>
@@ -82,18 +134,19 @@ const MyDocument = () => (
                             </div>
                             {/*body*/}
                             <div className="relative p-6 flex-auto">
-                              <Calendar selectRange={true} onChange={setCalendarRange}/>
+                              <Calendar selectRange={true} onChange={(value)=>{setCalendarRange(value)}}/>
                               <div className="flex items-center justify-end p-6 border-t border-solid border-blueGray-200 rounded-b">
-                                <button
-                                    className="text-red-500 hover:text-red-300 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-                                    type="button"
-                                    onClick={() => setCalendarToggle(false)}>
-                                    Close
-                                </button>
-                                <PDFDownloadLink document={<MyDocument />} 
-                                fileName="mypdf.pdf" className="bg-emerald-600 px-4 py-2 rounded text-white hover:bg-emerald-500 text-sm w-fit ms-auto">
+                              <button
+                                className="text-red-500 hover:text-red-300 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                                type="button"
+                                onClick={() => setCalendarToggle(false)}>
+                                Close
+                              </button>
+                              { ingredientsData.length > 0 ? (
+                                <PDFDownloadLink document={<MyDocument/>} fileName="mypdf.pdf" className="bg-emerald-600 px-4 py-2 rounded text-white hover:bg-emerald-500 text-sm w-fit ms-auto">
                                   {({ blob, url, loading, error }) => (loading ? 'Loading document...' : 'Download Diet')}
-                                </PDFDownloadLink>
+                                </PDFDownloadLink>  
+                                ) :(<button className="bg-emerald-600 px-4 py-2 rounded text-white hover:bg-emerald-500 text-sm w-fit ms-auto" onClick={fetchProductsInCalendarRange}>fetch data</button>) } 
                                 </div>
                             </div>
                             {/*footer*/}
