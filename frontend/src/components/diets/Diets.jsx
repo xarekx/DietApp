dc8006bbfffceccdb3a9d3d1513bea5cee735fcd
xarekx <arektarnowski4@gmail.com>
2024-07-07 13:16:00 +0200

    Corrected responsivness of diets page

--- frontend/src/components/diets/Diets.jsx
+++ frontend/src/components/diets/Diets.jsx
@@ -45,12 +45,53 @@ export function DietsData() {
         ));
       };
 
-    const generateDate = (dateArray, weekNumber) => {
+    const generateDate = (dateArray, dietsArray, weekNumber) => {
         const weekNum = parseInt(weekNumber.split(" ")[1], 10);
         const start = (weekNum - 1) * 7;
         const end = weekNum * 7;
         return dateArray.slice(start, end).map((item, index) => (
-            <span className="font-bold text-base rounded-md pb-2 pt-2 mb-4 border-[1px] border-slate-400" key={index}>{item}</span>
+            <>
+            <div className="flex flex-col">
+                <span className="font-bold text-base rounded-md pb-2 pt-2 mb-4 border-[1px] border-slate-400" key={index}>{item}</span>
+                <div className="flex flex-col border-dotted border-r-[1px] gap-y-4">
+                        
+                        <div className="flex lg:flex-col rounded shadow-lg md:text-xs border-[1px] border-slate-300">
+                            <img className="w-[100px] h-[75px] md:w-fit md:h-[98px] lg:w-full lg:h-full rounded-md" src="https://v1.tailwindcss.com/img/card-top.jpg" alt="Sunset in the mountains" />
+                            <div className="">
+                                <div className="font-semibold mt-2 m-1 mb-4 text-center md:p-0.5 lg:p-1 h-6 md:min-h-20 md:m-2 lg:m-1">{dietsArray[index]?.breakfast?.title || 'null'}</div>
+                            </div>
+                        </div>
+
+                        <div className="flex lg:flex-col rounded shadow-lg md:text-xs border-[1px] border-slate-300">
+                            <img className="w-[100px] h-[75px] md:w-fit md:h-[98px] lg:w-full lg:h-full rounded-md" src="https://v1.tailwindcss.com/img/card-top.jpg" alt="Sunset in the mountains" />
+                            <div className="">
+                                <div className="font-semibold mt-2 m-1 mb-4 text-center md:p-0.5 lg:p-1 h-6 md:min-h-20 md:m-2 lg:m-1">{dietsArray[index]?.second_breakfast?.title || 'null'}</div>
+                            </div>
+                        </div>
+
+                        <div className="flex lg:flex-col rounded shadow-lg md:text-xs border-[1px] border-slate-300">
+                            <img className="w-[100px] h-[75px] md:w-fit md:h-[98px] lg:w-full lg:h-full rounded-md" src="https://v1.tailwindcss.com/img/card-top.jpg" alt="Sunset in the mountains" />
+                            <div className="">
+                                <div className="font-semibold mt-2 m-1 mb-4 text-center md:p-0.5 lg:p-1 h-6 md:min-h-20 md:m-2 lg:m-1">{dietsArray[index]?.lunch?.title || 'null'}</div>
+                            </div>
+                        </div>
+
+                        <div className="flex lg:flex-col rounded shadow-lg md:text-xs border-[1px] border-slate-300">
+                            <img className="w-[100px] h-[75px] md:w-fit md:h-[98px] lg:w-full lg:h-full rounded-md" src="https://v1.tailwindcss.com/img/card-top.jpg" alt="Sunset in the mountains" />
+                            <div className="">
+                                <div className="font-semibold mt-2 m-1 mb-4 text-center md:p-0.5 lg:p-1 h-6 md:min-h-20 md:m-2 lg:m-1">{dietsArray[index]?.afternoon_meal?.title || 'null'}</div>
+                            </div>
+                        </div>
+
+                        <div className="flex lg:flex-col rounded shadow-lg md:text-xs border-[1px] border-slate-300">
+                            <img className="w-[100px] h-[75px] md:w-fit md:h-[98px] lg:w-full lg:h-full rounded-md" src="https://v1.tailwindcss.com/img/card-top.jpg" alt="Sunset in the mountains" />
+                            <div className="">
+                                <div className="font-semibold mt-2 m-1 mb-4 text-center md:p-0.5 lg:p-1 h-6 md:min-h-20 md:m-2 lg:m-1">{dietsArray[index]?.dinner?.title || 'null'}</div>
+                            </div>
+                        </div>                        
+                    </div>
+                </div>
+            </>
         ));
        
         
@@ -73,51 +114,8 @@ export function DietsData() {
                 <DietPlanPDF/>
                 <ShoppingListPDF/>
             </div>
-            <div className="grid lg:grid-cols-7 gap-y-8 gap-x-4 text-center md:ms-auto w-full mt-[4vh] ">
-            {generateDate(countWeeks.diet_days, selectedWeek)}   
-                {diets.map((diet, index) => {
-                    return(
-                        <>
-                        <div className="flex flex-col border-dotted border-r-[1px] gap-y-4" key={index}>  
-                                               
-                            <div className="flex lg:flex-col rounded shadow-lg md:text-xs border-[1px] border-slate-300">
-                                <img className="w-[100px] h-[75px] md:w-fit md:h-[98px] lg:w-full lg:h-full rounded-md" src="https://v1.tailwindcss.com/img/card-top.jpg" alt="Sunset in the mountains" />
-                                <div className="">
-                                    <div className="font-semibold mt-2 m-1 mb-4 text-center md:p-0.5 lg:p-1 h-6 md:min-h-20 md:m-2 lg:m-1">{diet.breakfast && diet.breakfast.title ? diet.breakfast.title : 'null'}</div>
-                                </div>
-                            </div>
-
-                            <div className="flex lg:flex-col rounded shadow-lg md:text-xs border-[1px] border-slate-300">
-                                <img className="w-[100px] h-[75px] md:w-fit md:h-[98px] lg:w-full lg:h-full rounded-md" src="https://v1.tailwindcss.com/img/card-top.jpg" alt="Sunset in the mountains" />
-                                <div className="">
-                                    <div className="font-semibold mt-2 m-1 mb-4 text-center md:p-0.5 lg:p-1 h-6 md:min-h-20 md:m-2 lg:m-1">{diet.second_breakfast && diet.second_breakfast.title ? diet.second_breakfast.title : 'null'}</div>
-                                </div>
-                            </div>
-
-                            <div className="flex lg:flex-col rounded shadow-lg md:text-xs border-[1px] border-slate-300">
-                                <img className="w-[100px] h-[75px] md:w-fit md:h-[98px] lg:w-full lg:h-full rounded-md" src="https://v1.tailwindcss.com/img/card-top.jpg" alt="Sunset in the mountains" />
-                                <div className="">
-                                    <div className="font-semibold mt-2 m-1 mb-4 text-center md:p-0.5 lg:p-1 h-6 md:min-h-20 md:m-2 lg:m-1">{diet.lunch && diet.lunch.title ? diet.lunch.title : 'null'}</div>
-                                </div>
-                            </div>
-
-                            <div className="flex lg:flex-col rounded shadow-lg md:text-xs border-[1px] border-slate-300">
-                                <img className="w-[100px] h-[75px] md:w-fit md:h-[98px] lg:w-full lg:h-full rounded-md" src="https://v1.tailwindcss.com/img/card-top.jpg" alt="Sunset in the mountains" />
-                                <div className="">
-                                    <div className="font-semibold mt-2 m-1 mb-4 text-center md:p-0.5 lg:p-1 h-6 md:min-h-20 md:m-2 lg:m-1">{diet.afternoon_meal && diet.afternoon_meal.title ? diet.afternoon_meal.title : 'null'}</div>
-                                </div>
-                            </div>
-
-                            <div className="flex lg:flex-col rounded shadow-lg md:text-xs border-[1px] border-slate-300">
-                                <img className="w-[100px] h-[75px] md:w-fit md:h-[98px] lg:w-full lg:h-full rounded-md" src="https://v1.tailwindcss.com/img/card-top.jpg" alt="Sunset in the mountains" />
-                                <div className="">
-                                    <div className="font-semibold mt-2 m-1 mb-4 text-center md:p-0.5 lg:p-1 h-6 md:min-h-20 md:m-2 lg:m-1">{diet.dinner && diet.dinner.title ? diet.dinner.title : 'null'}</div>
-                                </div>
-                            </div>
-                        </div>
-                        </>
-                    )
-                })}
+            <div className="grid lg:grid-cols-7 gap-y-8 gap-x-4 text-center md:ms-auto w-full mt-[4vh] ">            
+                {generateDate(countWeeks.diet_days, diets, selectedWeek)}
             </div>
         </div>
     </>
