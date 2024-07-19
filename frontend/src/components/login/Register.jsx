e5c5c107b3709e893bc61216b522d35bf695ff80
Arkadiusz Tarnowski <arektarnowski4@gmail.com>
2024-07-19 11:36:19 +0200

    Improved responsivness Login and Register Page

--- frontend/src/components/login/Register.jsx
+++ frontend/src/components/login/Register.jsx
@@ -1,6 +1,10 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useFetch } from "../../hooks/useFetch";
+import { Box, InputAdornment, TextField } from "@mui/material";
+import PersonOutlineOutlinedIcon from '@mui/icons-material/PersonOutlineOutlined';
+import LoginOutlinedIcon from '@mui/icons-material/LoginOutlined';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 
 export function UserRegister() {
 
@@ -31,26 +35,49 @@ export function UserRegister() {
     }
 
     return (
-    <div className="flex h-[70vh] justify-center items-center  me-auto ms-auto">
-        <div className="py-6 px-8 h-100 mt-20 bg-white rounded shadow-xl w-[85vw] sm:w-[65vw] md:w-[45vw] lg:w-[30vw] 2xl:w-[20vw]">
+    <div className="flex h-[70vh] justify-center items-center me-auto ms-auto">
+        <div className="py-6 px-4 h-100 mt-20 bg-white rounded shadow-xl w-[85vw] sm:w-[65vw] md:w-[45vw] lg:w-[30vw] 2xl:w-[20vw]">
+        <span className="flex justify-center text-base mb-4"><b>Create your Account</b></span>
         <form onSubmit={(e) => handleRegister(e)}>
-            <div>
-                <label htmlFor="email" className="block text-gray-800 font-bold">Email:</label>
-                <input type="text" name="email" id="email" placeholder="@email" className="w-full border border-gray-300 py-2 pl-3 rounded mt-2 outline-none focus:ring-indigo-600 :ring-indigo-600" onChange={e => setEmail(e.target.value)} />
-            </div>
-            <div>
-                <label htmlFor="email" className="block text-gray-800 font-bold">Username:</label>
-                <input type="text" name="username" id="username" placeholder="username" className="w-full border border-gray-300 py-2 pl-3 rounded mt-2 outline-none focus:ring-indigo-600 :ring-indigo-600" onChange={e => setUsername(e.target.value)} />
-            </div>
-            <div>
-                <label htmlFor="password" className="block text-gray-800 font-bold">Password</label>
-                <input type="password" name="password" id="password" placeholder="Password" className="w-full border border-gray-300 py-2 pl-3 rounded mt-2 outline-none focus:ring-indigo-600 :ring-indigo-600" onChange={e => setPassword(e.target.value)} />
-            </div>
-            <div className="mb-6">
-                <label htmlFor="repeat-password" className="block text-gray-800 font-bold">Password</label>
-                <input type="password" name="repeat-password" id="repeat-password" placeholder="Repeat password" className="w-full border border-gray-300 py-2 pl-3 rounded mt-2 outline-none focus:ring-indigo-600 :ring-indigo-600" onChange={e => setRepeatPassword(e.target.value)}/>
-            </div>
-            <button className="cursor-pointer py-2 px-4 block mt-6 bg-indigo-500 text-white font-bold w-full text-center rounded" type="submit">Sign Up</button>
+            <Box
+            sx={{
+                width: 300,
+                maxWidth: '100%',
+              }}>
+                <TextField fullWidth margin="normal" label="Email" id="email" variant="outlined" size="small" onChange={(e)=> setEmail(e.target.value)}
+                InputProps={{
+                    startAdornment: (
+                        <InputAdornment>
+                            <LoginOutlinedIcon/>
+                        </InputAdornment>
+                    )
+                }}/>
+                <TextField fullWidth margin="normal" label="Username" id="username" variant="outlined" size="small" onChange={(e)=> setUsername(e.target.value)}
+                InputProps={{
+                    startAdornment: (
+                        <InputAdornment>
+                            <PersonOutlineOutlinedIcon/>
+                        </InputAdornment>
+                    )
+                }}/>
+                <TextField fullWidth margin="normal" label="Password" id="Password" variant="outlined" size="small" type="password" onChange={(e)=> setPassword(e.target.value)}
+                InputProps={{
+                    startAdornment: (
+                        <InputAdornment>
+                            <LockOutlinedIcon/>
+                        </InputAdornment>
+                    )
+                }}/>
+                <TextField fullWidth margin="normal" label="Repeat Password" id="repeat-password" variant="outlined" size="small" type="password" onChange={(e)=> setRepeatPassword(e.target.value)}
+                InputProps={{
+                    startAdornment: (
+                        <InputAdornment>
+                            <LockOutlinedIcon/>
+                        </InputAdornment>
+                    )
+                }}/>
+            </Box>
+            <button className="cursor-pointer py-2 px-4 block mt-6 bg-indigo-500 hover:bg-indigo-600 text-white font-bold w-full text-center rounded" type="submit">Sign Up</button>
         </form>
         </div>
     </div>)
