fb34c162af183a19f53137ad7f05b398661ab1a8
Arek <arektarnowski4@gmail.com>
2026-09-17 14:57:26 +0200

    Unnecessary imports have been removed, option for automatic refreshing has been added in the docker-compose-dev

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,33 +1,15 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { PiBowlFoodFill } from "react-icons/pi";
 import { AppLayout } from "./layouts/AppLayout"
 import { AuthLayout } from "./layouts/AuthLayout"
 import { PublicLayout } from "./layouts/PublicLayout"
 import { LandingPage } from "./pages/public/LandingPage";
 import { Dashboard } from "./pages/app/Dashboard";
-import { ProductDetails } from "./components/products/ProductDetails";
-// import { ProductsData } from "./components/products/Products";
 import { Products } from "./pages/app/Products";
 import { RecipesData } from "./components/recipes/Recipes";
 import { DietsData } from "./components/diets/Diets";
-import { IoArrowDown } from "react-icons/io5";
-import { IoArrowForward } from "react-icons/io5";
-import { AddRecipeForm } from "./forms/AddRecipeForm";
-import { RecipeDetails } from "./components/recipes/RecipeDetails";
 import { UserLogin } from "./components/login/Login";
 import { UserRegister } from "./components/login/Register";
-import MenuIcon from '@mui/icons-material/Menu';
-import { getCookie } from "./utils/getCookie";
-import { useFetch } from "./hooks/useFetch";
-import { redirect } from "react-router-dom";
-import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
-import ListAltOutlinedIcon from '@mui/icons-material/ListAltOutlined';
-import BakeryDiningOutlinedIcon from '@mui/icons-material/BakeryDiningOutlined';
-import GridOnOutlinedIcon from '@mui/icons-material/GridOnOutlined';
-import AddBoxOutlinedIcon from '@mui/icons-material/AddBoxOutlined';
-
-import { CreateDietPlan } from "./components/diets/CreateDietPlan";
 
 function App() {
   return (
